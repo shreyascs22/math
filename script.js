3431aa7df9b2911fade1ca5511e491d5d97b2fb5
shreyascs22 <csshreyas22@gmail.com>
2023-10-25 21:45:22 +0530

    Styling calculator

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let output = document.getElementById("output");
 let expression = "";
-let result = "";
+let result = ""; // To save the final value before changing the expression or output
 
-//For showing 
+//For showing clicked button
 document.addEventListener("click",display);
 function display(element){
   if (element.target.classList.contains("calcterms")){
-    let buttonClicked = element.target.textContent;
+    let buttonClicked = element.target.textContent; //Getting value or content of the button clicked
     if (buttonClicked == 'x') {
       expression += '*';
     } 
@@ -20,14 +20,16 @@ function display(element){
   }
 }
 
+//For clear button
 clrbutton.addEventListener("click",clear);
-function clear(element){
+function clear(){
   expression = "";
   output.value = "";
 }
 
+//Final result or equal to(=) symbol operation
 equalsbutton.addEventListener("click",equals);
-function equals(element){
+function equals(){
   expression = expression.replace(/x/g, '*');
   expression = expression.replace(/÷/g, '/');
   expression = expression.replace(/\^/g, '**');
@@ -36,9 +38,27 @@ function equals(element){
   expression = String(result);
 }
 
+//For backspace
+bkspacebutton.addEventListener("click",backspace);
+function backspace(){
+  expression = expression.slice(0,expression.length - 1);
+  output.value = expression;
+}
+
+//For keyboard entry of values
 document.addEventListener("keydown", function(event) {
-  if (event.key.match(/[0-9]/)) {
+  const operators = ['+', '-', '*', '/', '^', '%', '.','(',')'];
+  if (event.key.match(/[0-9]/) || operators.includes(event.key)) {
     expression += event.key;
     output.value = expression;
+  }
+
+  if (event.key == "Enter"){
+    event.preventDefault();
+    equals();
+  }
+
+  if (event.key == "Backspace"){
+    backspace();
   }
 });
