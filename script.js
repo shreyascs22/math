00dc8119f4583c39e2d29371629bf5a6aa8a623e
shreyascs22 <csshreyas22@gmail.com>
2023-10-25 13:14:23 +0530

    calculating part

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-//const buttons = document.querySelectorAll(".calcterms");
 let output = document.getElementById("output");
 let expression = "";
+let result = "";
+
+//For showing 
 document.addEventListener("click",display);
 function display(element){
   if (element.target.classList.contains("calcterms")){
     let buttonClicked = element.target.textContent;
-    expression+=buttonClicked;
+    if (buttonClicked == 'x') {
+      expression += '*';
+    } 
+    else if (buttonClicked == '÷') {
+      expression += '/';
+    } 
+    else {
+      expression += buttonClicked;
+    }
     output.value = expression;
   }
 }
@@ -18,5 +28,17 @@ function clear(element){
 
 equalsbutton.addEventListener("click",equals);
 function equals(element){
-  
+  expression = expression.replace(/x/g, '*');
+  expression = expression.replace(/÷/g, '/');
+  expression = expression.replace(/\^/g, '**');
+  result = eval(expression);
+  output.value = result;
+  expression = String(result);
 }
+
+document.addEventListener("keydown", function(event) {
+  if (event.key.match(/[0-9]/)) {
+    expression += event.key;
+    output.value = expression;
+  }
+});
